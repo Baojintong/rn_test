b58184187e4e2677956bd8b872dd53fc43771f3f
baojintong <123488257@qq.com>
2017-10-30 17:57:34 +0800

    准备写个登录注册

--- Ignite2/BjtApp/App/Modules/Test/Containers/Login.js
+++ Ignite2/BjtApp/App/Modules/Test/Containers/Login.js
@@ -9,21 +9,6 @@ const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 class Billboard extends Component {
 
   constructor(props) {
-    // var userA = {
-    //   name: 'A',
-    //   age: 20,
-    //   tags: [
-    //     'geek',
-    //     'nerd',
-    //     'otaku'
-    //   ]
-    // };
-    // storage.save({
-    //   key: 'user',  // 注意:请不要在key中使用_下划线符号!
-    //   id: '1001',   // 注意:请不要在id中使用_下划线符号!
-    //   data: userA,
-    //   expires: 1000 * 60
-    // });
     super(props);
     this._refreshList();
     this.state = {
@@ -52,8 +37,8 @@ class Billboard extends Component {
     let name=data.name;
     return(
       <View style={styles.bookView}>
-          <Text>{name}</Text>
-          <Image style={styles.imageStyle} source={{uri:im_ui}}/>
+        <Text>{name}</Text>
+        <Image style={styles.imageStyle} source={{uri:im_ui}}/>
       </View>
     );
   }
@@ -102,21 +87,6 @@ class Billboard extends Component {
   }
 
 }
-
-//这个地方 state 后面. 出的是reducers(redux层注册的所有reducer的集合),
-// 原因在于在redux里面组件不直接更改state，全部依靠action，通过action确定到reducer，所以，新的state全部都是在redux层进行生成的，所以需要先确定
-// 在我们开发项目的时候；必定有多个reducer文件;使用combineEeducers方法将多个reducer合并成一个；
-
-// 下面这个state的来源
-// 我们知道一个reducer就会返回一个新的state;现在多个reducer合并成了一个reducer;下面又将这个总的reducer
-// 合并成了一个store;
-// const store = createStore(reducers);
-// store这个对象管理着多个reducer对应的state；形象点说；一个reducer对应一个state;然后通过combineReducers将多个state合并成了一个大的state对象; React-Redux 提供Provider组件;并设置参数store，可以让容器组件的connect方法拿到store。
-// <Provider store={store}>
-//   <App />
-// </Provider>,
-// 上面代码中，Provider在根组件外面包了一层，这样一来，App的所有子组件就默认都可以拿到state了。
-// 这个大的state那就是mapStateToProps方法的参数state的来源了；
 const mapStateToProps = (state) => {
   return {
     bookList: state.bookList.bookList,
